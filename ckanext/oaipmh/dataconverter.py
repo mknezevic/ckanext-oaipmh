77cddd243b548b63916c6aed816e096323a86839
mknezevic <knezevic.milica@gmail.com>
2014-02-03 23:17:31 +0100

    Update dataconverter.py

--- ckanext/oaipmh/dataconverter.py
+++ ckanext/oaipmh/dataconverter.py
@@ -2,16 +2,14 @@
 Contains code to convert metadata dictionary into form that's stored in CKAN
 database. Harvester would get a record in import_stage and pass it to this
 for storing the actual data in the database.
-
 In retrospect, there should be a mapping of XML paths to package and extra
 fields/keys which should handle the parsing. This code will become unwieldy as
 more sources with minor variations are added. Repeatability should be known.
 '''
-
 import logging
+log = logging.getLogger(__name__)
 import traceback
 import datetime
-
 from ckan import model
 from ckan.model import Package
 from ckan.model.authz import setup_default_user_roles
@@ -19,27 +17,19 @@ from ckan.model.license import LicenseRegister, LicenseOtherPublicDomain
 from ckan.model.license import LicenseOtherClosed, LicenseNotSpecified
 from ckan.controllers.storage import BUCKET, get_ofs
 #from ckanext.kata.utils import label_list_yso
-
 # used in label_list_yso()
 import urllib2
 import socket
 from lxml import etree
-
 # from ckan.lib.munge import munge_tag
 # from lxml import etree
-
 log = logging.getLogger(__name__)
-
-
 def oai_dc2ckan(data, namespaces, group=None, harvest_object=None):
     try:
         return _oai_dc2ckan(data, namespaces, group, harvest_object)
     except Exception as e:
         log.debug(traceback.format_exc(e))
-
     return False
-
-
 # Annoyingly, attribute such as rdf:about is presented with key such as
 # {http://www.w3.org/1999/02/22-rdf-syntax-ns#}about so we have to check the
 # end of the key. 
@@ -48,20 +38,14 @@ def _find_attribute(node, key_end):
         loc = key.find(key_end)
         if loc == len(key) - len(key_end):
             return node.get(key)
-
     return None
-
-
 # Given information about the license, try to match it with some known one.
 def _match_license(text):
     lr = LicenseRegister()
     for lic in lr.licenses:
         if text in (lic.url, lic.id, lic.title):
             return lic.id
-
     return None
-
-
 def _handle_title(nodes, namespaces):
     '''
     # :rtype : object
@@ -80,10 +64,7 @@ def _handle_title(nodes, namespaces):
         if lang:
             tl_dict['lang_title_%i' % idx] = lang
         idx += 1
-
     return tl_dict
-
-
 def _handle_rights(nodes, namespaces):
     d = {}
     lic_url_idx = 0
@@ -101,7 +82,6 @@ def _handle_rights(nodes, namespaces):
         else:  # Probably just old-fashioned text.
             text = node.text
             category = 'LICENSED'  # Let's give recognizing the license a try.
-
         if category == 'LICENSED' and text:
             lic = _match_license(text)
             if lic:
@@ -123,10 +103,7 @@ def _handle_rights(nodes, namespaces):
         elif category == 'COPYRIGHTED':
             lic = LicenseNotSpecified()
             d['package.license'] = {'id': lic.id}
-
     return d
-
-
 def _handle_contributor(nodes, namespaces):
     d = {}
     contr_idx = 0
@@ -147,10 +124,7 @@ def _handle_contributor(nodes, namespaces):
         elif node.text:  # Plain text field has none of the above.
             d['contributor_%i' % contr_idx] = node.text
             contr_idx += 1
-
     return d
-
-
 def _handle_publisher(nodes, namespaces):
     d = {}
     person_idx = 0
@@ -171,10 +145,7 @@ def _handle_publisher(nodes, namespaces):
             person_idx += 1
         # If not persons, then what is this? Apparently just text. Ignore?
         # Can be name of an organization.
-
     return d
-
-
 def _handle_format(nodes, namespaces):
     d = []
     for node in nodes:
@@ -204,15 +175,12 @@ def _handle_format(nodes, namespaces):
             if algorithm:
                 rd['extras'] = algorithm
             d.append(rd)
-
     return d
-
 # from https://github.com/kata-csc/ckanext-kata/blob/9a48369acf64f4eac0921d163787d1cfd22ababb/ckanext/kata/utils.py
 def label_list_yso(tag_url):
     """
     Takes tag keyword URL and fetches the labels that link to it.
     """
-
     _tagspaces = {
     'rdf' : 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
     'yso-meta' : 'http://www.yso.fi/onto/yso-meta/2007-03-02/',
@@ -228,7 +196,6 @@ def label_list_yso(tag_url):
     'xsd' : "http://www.w3.org/2001/XMLSchema#",
     'yso' : "http://www.yso.fi/onto/yso/",
     }
-
     labels = []
     if not tag_url.endswith("?rdf=xml"):
         tag_url += "?rdf=xml" # Small necessary bit.
@@ -251,20 +218,19 @@ def label_list_yso(tag_url):
                 if t:
                     labels.append(t)
     return labels
-
 def _oai_dc2ckan(data, namespaces, group, harvest_object):
     model.repo.new_revision()
     identifier = data['identifier']
-    metadata = data['metadata']
-    titles = _handle_title(metadata.get('titleNode', []), namespaces)
+    metadata_oai_dc = data['metadata']['oai_dc']
+    titles = _handle_title(metadata_oai_dc.get('titleNode', []), namespaces)
     # Store title in pkg.title and keep all in extras as well. That way
     # UI will work some way in any case.
     title = titles.get('title_0', identifier)
     #title = metadata['title'][0] if len(metadata['title']) else identifier
     name = data['package_name']
-    pkg = Package.get(name)
+    esc_identifier = identifier.replace('/','-')
+    pkg = Package.get(esc_identifier)
     if not pkg:
-        esc_identifier = identifier.replace('/','-')
         pkg = Package(name=name, title=title, id=esc_identifier)
         pkg.save()
         setup_default_user_roles(pkg)
@@ -277,7 +243,7 @@ def _oai_dc2ckan(data, namespaces, group, harvest_object):
     extras = titles
     idx = 0
     for s in ('subject', 'type'):
-        for tag in metadata.get(s, []):
+        for tag in metadata_oai_dc.get(s, []):
             # Turn each subject or type field into it's own tag.
             tagi = tag.strip()
             if tagi.startswith('http://www.yso.fi'):
@@ -304,76 +270,69 @@ def _oai_dc2ckan(data, namespaces, group, harvest_object):
                     pkgtag = model.PackageTag(tag=tag_obj, package=pkg)
                     pkgtag.save()  # Avoids duplicates if tags have duplicates.
     lastidx = 0
-    for auth in metadata.get('creator', []):
+    for auth in metadata_oai_dc.get('creator', []):
         extras['organization_%d' % lastidx] = ''
         extras['author_%d' % lastidx] = auth
         lastidx += 1
-    extras.update(_handle_contributor(metadata.get('contributorNode', []), namespaces))
-    extras.update(_handle_publisher(metadata.get('publisherNode', []), namespaces))
-
+    extras.update(_handle_contributor(metadata_oai_dc.get('contributorNode', []), namespaces))
+    extras.update(_handle_publisher(metadata_oai_dc.get('publisherNode', []), namespaces))
     # This value belongs to elsewhere.
     if 'package.maintainer_email' in extras:
         pkg.maintainer_email = extras['package.maintainer_email']
         del extras['package.maintainer_email']
-    extras.update(_handle_rights(metadata.get('rightsNode', []), namespaces))
+    extras.update(_handle_rights(metadata_oai_dc.get('rightsNode', []), namespaces))
     if 'package.license' in extras:
         pkg.license = extras['package.license']
         del extras['package.license']
-
     # Causes failure in commit for some reason.
     #for f in _handle_format(metadata.get('formatNode', []), namespaces):
     #    pprint.pprint(f)
     #    pkg.add_resource(**f)
-
     # There may be multiple identifiers (URL, ISBN, ...) in the metadata.
     id_idx = 0
-    for ident in metadata.get('identifier', []):
+    for ident in metadata_oai_dc.get('identifier', []):
         extras['identifier_%i' % id_idx] = ident
         id_idx += 1
-
     # Check that we have a language.
-    lang = metadata.get('language', [])
+    lang = metadata_oai_dc.get('language', [])
     if lang and len(lang) and len(lang[0]) > 1:
         pkg.language = lang[0]
-
-    # The rest.
-    # description below goes to pkg.notes. I think it should not added here.
-    for key, value in metadata.items():
-        if value is None or len(value) == 0 or key in ('titleNode', 'subject', 'type', 'rightsNode',
-                                                       'publisherNode', 'creator', 'contributorNode',
-                                                       'description', 'identifier', 'language', 'formatNode'):
-            continue
-        extras[key] = ' '.join(value)
-    #description = metadata['description'][0] if len(metadata['description']) else ''
-    notes = ' '.join(metadata.get('description', []))
-    pkg.notes = notes.replace('\n', ' ').replace('  ', ' ')
-
     if 'date' in extras:
         pkg.version = extras['date']
         del extras['date']
-
     pkg.extras = extras
     pkg.url = data['package_url']
-    if 'package_resource' in data:
+    
+    # Metadata may have different identifiers, pick link, if exists.
+    for ids in metadata_oai_dc['identifier']:
+        if ids.startswith('http://') or ids.startswith('https://'):
+            pkg.add_resource(ids, name=pkg.title, format='html')
+    # All belong to the main group even if they do not belong to any set.
+    if group:
+        group.add_package_by_name(pkg.name)
+    # The rest.
+    # description below goes to pkg.notes. I think it should not added here.
+    for mdp, metadata in data['metadata'].items():
+        for key, value in metadata.items():
+            if value is None or len(value) == 0 or key in ('titleNode', 'subject', 'type', 'rightsNode',
+                                                           'publisherNode', 'creator', 'contributorNode',
+                                                           'description', 'identifier', 'language', 'formatNode'):
+                continue
+            extras[key] = ' '.join(value)
+        #description = metadata['description'][0] if len(metadata['description']) else ''
+        notes = ' '.join(metadata.get('description', []))
+        pkg.notes = notes.replace('\n', ' ').replace('  ', ' ')
+    
+    for mdp, resource in data['package_resource'].items():
         ofs = get_ofs()
-        ofs.put_stream(BUCKET, data['package_xml_save']['label'], data['package_xml_save']['xml'], {})
-        pkg.add_resource(**(data['package_resource']))
+        ofs.put_stream(BUCKET, data['package_xml_save'][mdp]['label'], data['package_xml_save'][mdp]['xml'], {})
+        pkg.add_resource(**(resource))
+    
     if harvest_object:
         harvest_object.package_id = pkg.id
         harvest_object.content = None
         harvest_object.current = True
         harvest_object.save()
-
-    # Metadata may have different identifiers, pick link, if exists.
-    for ids in metadata['identifier']:
-        if ids.startswith('http://') or ids.startswith('https://'):
-            pkg.add_resource(ids, name=pkg.title, format='html')
-
-    # All belong to the main group even if they do not belong to any set.
-    if group:
-        group.add_package_by_name(pkg.name)
-
+    
     model.repo.commit()
-
     return pkg.id
-
